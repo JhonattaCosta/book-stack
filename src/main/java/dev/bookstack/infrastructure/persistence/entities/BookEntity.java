c85edd6f962500ff6898428074cfc9f33a16a27b
Jhonatta Costa <jhonattasantos27@gmail.com>
2026-02-13 20:20:15 -0300

    feat: add BookEntity in Infra/Persistence

---
+++ src/main/java/dev/bookstack/infrastructure/persistence/entities/BookEntity.java
@@ -0,0 +1,35 @@
+package dev.bookstack.infrastructure.persistence.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "book")
+@NoArgsConstructor
+@Getter
+@Setter
+public class BookEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String author;
+    @Column(nullable = false)
+    private String category;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    @Column(nullable = false)
+    private String Isbn;
+
+}
