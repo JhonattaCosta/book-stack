0083569ed8bbc480970f3da0a5d4669a7c73acf4
Jhonatta Costa <jhonattasantos27@gmail.com>
2026-02-14 17:50:50 -0300

    fix: remove @NotBlank validations from UpdateBookRequest

--- src/main/java/dev/bookstack/application/dto/request/UpdateBookRequestDto.java
+++ src/main/java/dev/bookstack/application/dto/request/UpdateBookRequestDto.java
@@ -3,12 +3,8 @@ package dev.bookstack.application.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record UpdateBookRequestDto (
-        @NotBlank
         String title,
-        @NotBlank
         String author,
-        @NotBlank
         String category,
-        @NotBlank
         String isbn){
 }
