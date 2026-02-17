feb8b33b6bdbea85605b27d2d8b3bb6a5d7489ba
Jhonatta Costa <jhonattasantos27@gmail.com>
2026-02-17 19:13:09 -0300

    feat: implement FindBookByAuthorUseCase with TDD

---
+++ src/main/java/dev/bookstack/application/usecases/book/FindBookByAuthorUseCase.java
@@ -0,0 +1,22 @@
+package dev.bookstack.application.usecases.book;
+
+import dev.bookstack.application.dto.response.BookResponseDto;
+import dev.bookstack.application.mappers.BookMapper;
+import dev.bookstack.domain.repositories.BookRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FindBookByAuthorUseCase {
+
+    private final BookRepository repository;
+
+    public FindBookByAuthorUseCase(BookRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<BookResponseDto> execute(String author){
+        return repository.findByAuthor(author).stream().map(BookMapper::toResponse).toList();
+    }
+}
