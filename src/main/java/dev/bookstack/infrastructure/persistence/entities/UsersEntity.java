94007641f93afc46c62e6b73aa49ccd7baf33e6e
Jhonatta Costa <jhonattasantos27@gmail.com>
2026-03-01 17:31:53 -0300

    feat: Add UsersEntity for persistence in the DB

---
+++ src/main/java/dev/bookstack/infrastructure/persistence/entities/UsersEntity.java
@@ -0,0 +1,35 @@
+package dev.bookstack.infrastructure.persistence.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UsersEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String cpf;
+    private boolean isAdmin;
+    private boolean isActive;
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+}
